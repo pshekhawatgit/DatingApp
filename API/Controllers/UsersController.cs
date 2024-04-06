bfe3f78e3022ae609a69b5b56f0c0ae9a3286c99
pshekhawatgit <pradyumnashekhawat@gmail.com>
2024-04-06 15:15:10 -0400

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers;
+[ApiController]
+[Route("API/[Controller]")] // https://api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet] // https://api/users
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")] // https://api/users/2
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
