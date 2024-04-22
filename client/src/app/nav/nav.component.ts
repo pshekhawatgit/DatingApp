fe80ae40ecc043bec13527296bd31ce668fece4a
pshekhawatgit <pradyumnashekhawat@gmail.com>
2024-04-22 19:55:32 -0400

    Enable routing on UI and component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,21 +12,20 @@ import { User } from '../_models/user';
 export class NavComponent {
   model: any = {}
 
-  constructor(public accountService: AccountService){} // Made public for accessing in html
+  constructor(public accountService: AccountService, private router: Router){} // Made public for accessing in html
   
   ngOnInit(): void{
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
+      next: _ => this.router.navigateByUrl('/members'), // Instead of _ you can also use ()
       error: error => console.log(error)
   })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
