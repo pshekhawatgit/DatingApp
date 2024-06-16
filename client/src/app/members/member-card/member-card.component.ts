7c637a2ab2ae6a19a6c38315ffd027da4359eade
pshekhawatgit <pradyumnashekhawat@gmail.com>
2024-06-16 08:37:46 -0400

    Added styling to the cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // To encapsulate/contain the styles from css, this is also the default value
 })
 export class MemberCardComponent {
   @Input() member: Member | undefined; // To use data from Parent (member-list) component, set to undefined to initialize
