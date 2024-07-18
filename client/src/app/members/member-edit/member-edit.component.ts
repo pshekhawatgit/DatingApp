1b70e5ec763bef7a79d2c85f16f39224afe71ddb
pshekhawatgit <pradyumnashekhawat@gmail.com>
2024-07-17 20:00:21 -0400

    Added route guards to prevent unsaved changes

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
@@ -14,6 +14,11 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm | undefined;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any){ // Code to warn user while navigating away from page with unsaved edits
+    if(this.editForm?.dirty){
+      $event.returnValue = true;
+    }
+  }
   member: Member | undefined;
   user: User | undefined | null;
 
