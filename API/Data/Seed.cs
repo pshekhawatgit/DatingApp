93c9997b666dbf2e69138acfdd74ee8802975b4d
pshekhawatgit <pradyumnashekhawat@gmail.com>
2024-11-13 20:19:05 -0500

    Identity & Role: Add Roles

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -10,7 +10,7 @@ namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
         // Return if there are any Users already in DB
         if(await userManager.Users.AnyAsync())
@@ -23,12 +23,37 @@ public class Seed
         var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
         // Deserialize from json String to C# object
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
+        // Create roles
+        var roles = new List<AppRole>
+        {
+            new AppRole{Name = "Member"},
+            new AppRole{Name = "Admin"},
+            new AppRole{Name = "Moderator"}
+        };
+
+        // Sabe all roles in DB
+        foreach(var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+
         // Get UserName and Generate Password for each user and add it to DB context
         foreach(var user in users)
         {
             user.UserName = user.UserName.ToLower();
 
             await userManager.CreateAsync(user, "Pa$$w0rd");
+            // map to a role
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        // Create Admin user
+        var admin = new AppUser{
+            UserName = "admin"
+        };
+        // save admin in DB
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        // Map multiple roles to admin
+        await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
     }
 }
