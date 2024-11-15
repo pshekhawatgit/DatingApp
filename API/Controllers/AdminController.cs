1081ddf7924feff5cb8325461894b114cb003672
pshekhawatgit <pradyumnashekhawat@gmail.com>
2024-11-14 19:31:53 -0500

    Identity & Role: Get user with roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,16 +1,35 @@
 using System;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AdminController : BaseApiController
 {
+    private readonly UserManager<AppUser> _userManager;
+
+    public AdminController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRoles()
+    public async Task<ActionResult> GetUsersWithRoles()
     {
-        return Ok("If you see this, you are an Admin");
+        var users = await _userManager.Users
+            .OrderBy(u => u.UserName)
+            .Select(u => new // return a new Object with Properties we want to return
+            {
+                u.Id,
+                Username = u.UserName,
+                Roles = u.UserRoles.Select(ur => ur.Role.Name) // We only need Role Names
+            }).ToListAsync();
+
+        return Ok(users);
     }
 
     [Authorize(Policy = "ModeratePhotoRole")]
