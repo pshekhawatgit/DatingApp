3528d8d00d454f0dfcc8762f1dd320c026211b99
pshekhawatgit <pradyumnashekhawat@gmail.com>
2024-11-19 18:31:07 -0500

    SignalR: Update message hub with group tracking

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -6,6 +6,7 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace API.SignalR;
 
@@ -25,6 +26,7 @@ public class MessageHub(IMessageRepository messageRepository, IUserRepository us
         // get groupname and add to groups
         var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
         await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        await AddToGroup(groupName);
 
         // get message thread between these users
         var messages = await messageRepository.GetMessagesThread(Context.User.GetUsername(), otherUser);
@@ -34,9 +36,10 @@ public class MessageHub(IMessageRepository messageRepository, IUserRepository us
     }
 
     //  Override the HUB method, When a user disconnects from HUB
-    public override Task OnDisconnectedAsync(Exception exception)
+    public override async Task OnDisconnectedAsync(Exception exception)
     {
-        return base.OnDisconnectedAsync(exception);
+        await RemoveFromMessageGroup();
+        await base.OnDisconnectedAsync(exception);
     }
 
     // To send message from Message Hub (SignalR), instead of using Controller
@@ -64,11 +67,19 @@ public class MessageHub(IMessageRepository messageRepository, IUserRepository us
                 Content = createMessageDto.Content
             };
 
+            // Check if two users are in same group, so that messages can be marked "read" if both are chatting
+            var groupName = GetGroupName(sender.UserName, recipient.UserName);
+            var group = await messageRepository.GetMessageGroup(groupName);
+            // If recipient is in the group
+            if(group.connections.Any(x => x.Username == recipient.UserName))
+            {
+                message.DateRead = DateTime.UtcNow;
+            }
+
             messageRepository.AddMessage(message);
 
             if(await messageRepository.SaveAllAsync())
             {
-                var groupName = GetGroupName(sender.UserName , recipient.UserName);
                 await Clients.Group(groupName).SendAsync("NewMessage", mapper.Map<MessageDto>(message));
             }
     }
@@ -78,5 +89,32 @@ public class MessageHub(IMessageRepository messageRepository, IUserRepository us
         var stringCompare = string.CompareOrdinal(caller, other) < 0;
 
         return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+
+    private async Task<bool> AddToGroup(string groupName){
+        // Check if group already exists
+        var group = await messageRepository.GetMessageGroup(groupName);
+        // If not
+        if(group == null)
+        {
+            group = new Group(groupName);
+            messageRepository.AddGroup(group);
+        }
+
+        // Create Connection object using connection details from HUBContext
+        var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
+        group.connections.Add(connection);
+        
+        return await messageRepository.SaveAllAsync();
+    }
+
+    private async Task RemoveFromMessageGroup()
+    {
+        // Get connection
+        var connection = await messageRepository.GetConnection(Context.ConnectionId);
+        // Remove connection
+        messageRepository.RemoveConnection(connection);
+        // Save to DB
+        await messageRepository.SaveAllAsync();
     }
 }
